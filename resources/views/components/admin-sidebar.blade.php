dfb6201edc537a9231b5b32f9baef7a3747ab061
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-02-06 13:16:53 +0545

    Updated Company Dashboard layout in admin sidebar

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -1,22 +1,40 @@
 <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-        <a href="index.html"> <img alt="image" src="assets/img/logo.png" class="header-logo" /> <span
+        <a href="{{ route('dashboard') }}"> <img alt="image" src="{{ asset('assets/img/images.jpeg') }}" class="header-logo w-36" /> <span
                 class="logo-name">Otika</span>
         </a>
     </div>
     <ul class="sidebar-menu">
         <li class="menu-header">Main</li>
         <li class="dropdown active">
-            <a href="index.html" class="nav-link"><i
-                    data-feather="monitor"></i><span>Dashboard</span></a>
+            <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
         </li>
         <li class="dropdown">
+            <a href="{{ route('admin.company.index') }}" class="nav-link"><i class="
+fas fa-building"></i><span>Company</span></a>
+        </li>
+        <li class="dropdown">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i class="
+fab fa-angellist"></i><span>Category</span></a>
+        </li>
+        <li class="dropdown">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i class="
+far fa-newspaper"></i><span>Article</span></a>
+        </li>
+        <li class="dropdown">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i class="
+fas fa-camera-retro"></i><span>Advertise</span></a>
+        </li>
+
+
+
+        {{-- <li class="dropdown">
             <a href="#" class="menu-toggle nav-link has-dropdown"><i
                     data-feather="briefcase"></i><span>Widgets</span></a>
             <ul class="dropdown-menu">
                 <li><a class="nav-link" href="widget-chart.html">Chart Widgets</a></li>
                 <li><a class="nav-link" href="widget-data.html">Data Widgets</a></li>
             </ul>
-        </li>
+        </li> --}}
     </ul>
 </aside>
