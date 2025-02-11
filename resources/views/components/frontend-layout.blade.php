84eb4ddb31d90028366264b6a3d14ef3e6bb527a
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-02-11 16:57:41 +0545

    Create frontend layout blade view

---
+++ resources/views/components/frontend-layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('frontend/style.css') }}">
+</head>
+<body>
+    <header></header>
+
+    <main>
+        {{ $slot }}
+    </main>
+
+    <footer></footer>
+</body>
+</html>
